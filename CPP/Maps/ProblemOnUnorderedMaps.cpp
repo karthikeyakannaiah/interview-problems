b3e8cf7b37d80a88b858eb8a8b4369c0f7081262
Kannaiah Karthikeya <50999627+karthikeyakannaiah@users.noreply.github.com>
2022-08-09 22:26:41 +0530

    Create ProblemOnUnorderedMaps.cpp

---
+++ CPP/Maps/ProblemOnUnorderedMaps.cpp
@@ -0,0 +1,13 @@
+/*
+  Given N strings and Q queries
+  In each each query you are given a string
+  print frequency of that string
+  N <= 10^6
+ |S| <= 100
+  Q <= 10^6
+*/
+
+/*
+    Use Unordered maps incase if you dont need lexicographical ordering
+    It will save time
+*/
