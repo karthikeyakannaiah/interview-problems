dc5b37b9e97277206129bc7c197830fe5ce00e68
Kannaiah Karthikeya <50999627+karthikeyakannaiah@users.noreply.github.com>
2022-08-12 17:00:47 +0530

    Create O(n).cpp

---
+++ CPP/ESSENTIALS/TimeComplexity/O(n).cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+// N < 100000
+// a[i] < 100000
+
+int main(){
+    int n; // --> 1
+    cin >> n; // --> 1
+    for (int i = 0; i<n; i++){  // --> n
+        cin >> a[i]; // --> 1
+    }
+    for (int i = 0; i<n; i++){  // --> n
+        cout << a[i] << endl;  // --> 1
+    }
+}
+// TIME COMPLEXITY O(n)
+/*
+    1+1+n+n= 2+ 2*n
+
+    T(n)=2*(1+n) ----==+> O(n)
+
+*/
