983b95a979f1c4c5b4c04b8c95b7d575050189e7
Kannaiah Karthikeya <50999627+karthikeyakannaiah@users.noreply.github.com>
2022-08-21 16:19:38 +0530

    Create findAllSymmetricPairs.cpp

---
+++ PracticeProblems/tcs-practice-sheet/findAllSymmetricPairs.cpp
@@ -0,0 +1,23 @@
+// referred solution
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n = 5;
+	int arr[5][2] = {{1, 2}, {2, 1}, {3, 4}, {4, 5}, {5, 4}};
+	unordered_map<int, int>mp;
+	cout<<"The Symmetric Pairs are: "<<endl;
+	for (int i = 0; i < n; i++) {
+		int first = arr[i][0];
+		int second = arr[i][1];
+		
+		//if {second,first} existed previously,print them.
+		if (mp.find(second) != mp.end() && mp[second] == first) {
+			cout << "(" << first << " " << second << ")" << " ";
+		}
+		//else store them in the map
+		else {
+			mp[first] = second;
+		}
+	}
+}
