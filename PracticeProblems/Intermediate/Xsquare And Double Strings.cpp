6ac99890a5737c7785c5bb311c14051b15e819a9
Kannaiah Karthikeya <50999627+karthikeyakannaiah@users.noreply.github.com>
2022-08-22 18:55:26 +0530

    Create Xsquare And Double Strings.cpp

---
+++ PracticeProblems/Intermediate/Xsquare And Double Strings.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    while(n--){
+        string s;
+        cin >> s;
+        int a[26]={0};
+        for(int i=0; i<s.length(); ++i){
+            a[s[i]-'a']++;
+        }
+        int count=0;
+        for(int i=0; i<26; ++i){
+            if(a[i]>=2){
+                count++;
+                break;
+            }
+        }
+        if(count==1){
+            cout << "Yes" << endl;
+        }else{
+            cout << "No" << endl; 
+        }      
+    }
+}
+/*
+INPUT
+5
+wow
+tata
+a
+ab
+lala
+
+OUTPUT
+YES
+YES
+NO
+NO
+YES
+*/
